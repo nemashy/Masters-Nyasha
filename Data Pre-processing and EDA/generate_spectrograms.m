92efeef7b238a0c81254d677ddda978f92264161
NyashaMash <49335794+nemashy@users.noreply.github.com>
2021-08-03 12:49:18 +0200

    Change file name to preprocess.m

--- Data Pre-processing and EDA/generate_spectrograms.m
+++ Data Pre-processing and EDA/generate_spectrograms.m
@@ -5,14 +5,14 @@ function spectograms_struct = generate_spectrograms(trkdata, window_length, over
         for range_bin_pos = 1:length(trkdata) % range_bin_pos = 16
             fs_Hz = trkdata(range_bin_pos).PRF; % sampling frequency = PRF
             [IQ_samples, label] = get_IQ_samples(range_bin_pos, trkdata);
-            if(nargin == 5) % If there is filter
+            if(nargin == 5) % If there is a filter
                 IQ_samples = filter(filter_params,IQ_samples);
             end
             % save('nyasha_signal.mat','IQ_samples', 'label');
             window = hamming(window_length);
             overlap_length = overlap_fraction * window_length;
             % Get STFT of the example
-            [S, F, T] = stft(IQ_samples,fs_Hz,'Window',window,'OverlapLength',overlap_length,'FFTLength',fftLength);
+            [S, ~, T] = stft(IQ_samples,fs_Hz,'Window',window,'OverlapLength',overlap_length,'FFTLength',fftLength);
             S_dB = 20*log10(abs(S));
             spectograms_struct(range_bin_pos).Data = S_dB;
             spectograms_struct(range_bin_pos).Label = label;
