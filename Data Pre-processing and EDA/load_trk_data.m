70ad905ec355c4324ca192659725678ab513748c
NyashaMash <49335794+nemashy@users.noreply.github.com>
2021-08-03 18:43:52 +0200

    Delete Data Pre-processing and EDA directory

--- Data Pre-processing and EDA/load_trk_data.m
+++
@@ -1,8 +0,0 @@
-function [trk_data] = load_trk_data(data_dir,file_name)
-% This function loads the raw data stored in .mat files
-% Each .mat file corresponds to a certain time
-    file_path = strcat(data_dir,'\',file_name);
-    trk_data_struct = load(file_path);
-    trk_data = trk_data_struct.trkdata;
-end
-
