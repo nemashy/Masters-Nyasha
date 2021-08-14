2b6963324049fa0bcae88baab6dac3cb033b8b4b
Nyasha <nyasha@peralex.com>
2021-08-14 10:09:30 +0200

    Measure the number of supportable range bins

--- Preprocessing/main.m
+++ Preprocessing/main.m
@@ -11,7 +11,7 @@ trk_data_struct_files = dir(fullfile(data_dir,'*.mat'));
 
 tic % start timer
 for iFile = 1:length(trk_data_struct_files)
-    file_name = trk_data_struct_files(iFile).name; %file_name = trk_data_struct_files(60).name;
+    file_name = trk_data_struct_files(iFile).name; %trk_data_struct_files(60).name; 
     trk_data_struct = load_trk_data(data_dir, file_name);
     stft_data_struct = get_stft_data(trk_data_struct, window_length, overlap_fraction, fft_length, filter_params);
     examples_struct = generate_processed_examples(stft_data_struct, dwell_time, example_overlap_fraction);
