1b9b3dd9cf37f7d5e6518eada4f452e42a4227ee
nemashy <nemashy@gmail.com>
2021-08-15 20:57:33 +0200

    Deleted data analysis

--- training/model_training.py
+++ training/model_training.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
+import random
 import time
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -305,6 +306,29 @@ def get_info(optimizer):
     }
 
     return optimizer_name, optimizer_params_dict
+
+def display_errors(img_errors, pred_errors, obs_errors, classes, class_of_interest=None):
+    """ This function shows 6 images with their predicted and real labels"""
+    n = 0
+    nrows = 2
+    ncols = 5
+
+    if class_of_interest is not None:
+        # Select a specific class
+        img_errors = img_errors[np.where(obs_errors==class_of_interest)]
+        pred_errors = pred_errors[np.where(obs_errors==class_of_interest)]
+        obs_errors = obs_errors[np.where(obs_errors==class_of_interest)]
+
+    errors_idxs = random.sample(range(0, len(img_errors)), len(img_errors))
+
+    fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True, figsize=(14,10))
+    for row in range(nrows):
+        for col in range(ncols):
+            error = errors_idxs[n]
+            ax[row,col].imshow((img_errors[error]).reshape((128,45)), cmap='turbo')
+            ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format( classes[int(pred_errors[error])], classes[int(obs_errors[error])]  ))
+            n += 1
+
 
 def main():
     pass
