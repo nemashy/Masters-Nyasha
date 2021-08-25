Correct train loss calculation

training/inference.py
@@ -42,7 +42,11 @@ class ModelPerformance:
         """Returns the total inference time on all the loops"""
         # Wait for all kernels in all streams on the CUDA device to complete.
         torch.cuda.current_stream().synchronize()
-
+        
+        # GPU warmup
+        for _ in range(10):
+            _ = model(self.input_data_batch)
+        
         t0 = time.time()
         for _ in range(num_loops):
             _ = model(self.input_data_batch)

training/model_training.py
@@ -124,14 +124,15 @@ class ModelTrainer:
                 loss.backward()
                 optimizer.step()
 
-                train_loss += loss.item() / images.shape[0]
+                train_loss += loss.item() * images.shape[0]
                 accuracy = 100 * correct / total
+
 
                 train_loss_per_batch[batch_idx] = train_loss
                 accuracy_per_batch[batch_idx] = accuracy
 
-            avg_epoch_train_loss = np.mean(train_loss_per_batch)
-            avg_epoch_accuracy = np.mean(accuracy_per_batch)
+            epoch_train_loss = train_loss / len(self.train_loader.dataset)
+            epoch_train_accuracy = correct * 100/ len(self.train_loader.dataset)
 
             val_loss, val_accuracy, _, _, _ = self.evaluate_model(
                 self.model,
@@ -141,11 +142,11 @@ class ModelTrainer:
             if self.scheduler is not None:
                 self.scheduler.step()
 
-            write_to_tensorboard(writer, avg_epoch_train_loss, avg_epoch_accuracy, val_loss, val_accuracy, epoch)
+            write_to_tensorboard(writer, epoch_train_loss, epoch_train_accuracy, val_loss, val_accuracy, epoch)
 
             print(
-                "Epoch: {}/{} \t Training Loss: {:.4f}, Accuracy: {:.2f}, Testing Loss: {:.4f}, Accuracy: {:.2f}".format(
-                    epoch, num_epochs, avg_epoch_train_loss, avg_epoch_accuracy, val_loss, val_accuracy
+                "Epoch: {}/{} \t Training Loss: {:.4f}, Accuracy: {:.2f} %, Validation Loss: {:.4f}, Accuracy: {:.2f} %".format(
+                    epoch, num_epochs, epoch_train_loss, epoch_train_accuracy, val_loss, val_accuracy
                 )
             )
 
@@ -192,7 +193,7 @@ class ModelTrainer:
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-                running_loss += loss.item()
+                running_loss += loss.item() * images.shape[0]
 
                 labels = labels.cpu()
                 predicted = predicted.cpu()
@@ -201,7 +202,7 @@ class ModelTrainer:
                 y_pred_tot = torch.cat((y_pred_tot, predicted), 0)
 
         accuracy = 100 * correct / total
-        accuracy = 100 * correct / total
+        val_loss = running_loss / len(data_loader.dataset)
         errors = y_pred_tot - y_tot != 0
         y_pred_errors = y_pred_tot[errors]
         y_true_errors = y_tot[errors]
@@ -210,7 +211,7 @@ class ModelTrainer:
         if show_cm:
             self.generate_confusion_matrix(self.classes, y_tot, y_pred_tot)
 
-        return running_loss / num_batches, accuracy, errors, y_pred_errors, y_true_errors
+        return val_loss, accuracy, errors, y_pred_errors, y_true_errors
 
     @staticmethod
     def generate_confusion_matrix(classes, y_tot, y_pred_tot):
@@ -340,9 +341,9 @@ def get_class_weights(y_train, device):
     return class_weights
 
 
-def write_to_tensorboard(writer, avg_epoch_train_loss, avg_epoch_accuracy, val_loss, val_accuracy, epoch):
-    writer.add_scalar("Loss/Train", avg_epoch_train_loss, epoch)
-    writer.add_scalar("Accuracy/Train", avg_epoch_accuracy, epoch)
+def write_to_tensorboard(writer, epoch_train_loss, epoch_train_accuracy, val_loss, val_accuracy, epoch):
+    writer.add_scalar("Loss/Train", epoch_train_loss, epoch)
+    writer.add_scalar("Accuracy/Train", epoch_train_accuracy, epoch)
     writer.add_scalar("Loss/Validation", val_loss, epoch)
     writer.add_scalar("Accuracy/Validation", val_accuracy, epoch)
 

training/prune.py
@@ -73,12 +73,14 @@ def prune_ernet(model, sparsity):
 def measure_accuracy_ernet(model, criterion, data_loaders_and_classes,model_type):
     """Measure accuracy of a model for different sparsities"""
     model.to('cuda')
-    for sparsity in np.arange(0,0.91,0.01):
+    for sparsity in np.arange(0,0.81,0.01):
         model_copy = copy.deepcopy(model)
         if model_type=='ernet':
             pruned_model = prune_ernet(model_copy, sparsity)
         elif model_type=='resnet':
             pruned_model = prune_resnet(model_copy, sparsity)
+        if sparsity==0.19:
+            torch.save(pruned_model, 'pruned_model.pth')
 
         model_trainer = ModelTrainer(pruned_model, criterion, data_loaders_and_classes)
         
@@ -95,7 +97,7 @@ def plot_results(results):
     # y2 = np.cos(2 * np.pi * x2)
     x, y = zip(*results)
     fig, (ax1) = plt.subplots(1, 1)
-    fig.suptitle('A tale of 2 subplots')
+    fig.suptitle('Results of pruning a model')
 
     ax1.plot(x, y, '-')
     ax1.set_xlabel('Sparsity (%)')
@@ -112,17 +114,18 @@ def main():
 
     device = get_device()
 
-    model_ft = models.resnet18(pretrained=False)
-    num_ftrs = model_ft.fc.in_features
-    model_ft.fc = nn.Linear(num_ftrs, 6)
-    model_ft.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+    # model_ft = models.resnet18(pretrained=False)
+    # num_ftrs = model_ft.fc.in_features
+    # model_ft.fc = nn.Linear(num_ftrs, 6)
+    # model_ft.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 
-    weights_path = 'training/checkpoint.pt'
-    model = get_pytorch_model(weights_path, model_ft) # ErnNet()
+    weights_path = 'training/model_ckpt/ernet.pt'
+   
         # Initialise model
-    # model = ErnNet()
-    # model = get_pytorch_model('checkpoint.pt', model)
+    model = ErnNet()
+
     # Initialising training parameters
+    model = get_pytorch_model(weights_path, model) # ErnNet()
     class_weights = get_class_weights(data['y_train'], device)
     criterion = nn.CrossEntropyLoss(weight=class_weights)
     optimizer = optim.Adam(model.parameters(), lr)
@@ -130,7 +133,7 @@ def main():
     # Scheduling parameters
     scheduler = Scheduler(optimizer, transition_steps, gamma)
     lr_scheduler = scheduler.get_MultiStepLR()
-    results = measure_accuracy_ernet(model, criterion, data_loaders_and_classes,'resnet')
+    results = measure_accuracy_ernet(model, criterion, data_loaders_and_classes,'ernet')
     # for sparsity, accuracy in results:
     #     print(sparsity, accuracy)
 